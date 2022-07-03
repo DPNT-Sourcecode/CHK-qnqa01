0df9fa7d32489140c167a73e3b370b03f74d8b8e
sbx_user1051 <sbx_user1051@169.254.105.205>
2022-07-03 14:17:06 +0000

    Sun Jul 03 14:17:06 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -31,12 +31,20 @@ def checkout(skus):
     for key, val in sku_groups.items():
         if not key in available_skus:
             return -1
+        
         current_sum = 0
-        if val in special_offers:
-            
+        if key in special_offers:
+            even_num = val // special_offers[key]['num']
+
+            # calculate the special offers first
+            current_sum += even_num * special_offers[key]['price']
+
+            # calculate others
+            current_sum += (val - even_num) * available_skus[key]
 
 
     return -1
+
 
 
 
