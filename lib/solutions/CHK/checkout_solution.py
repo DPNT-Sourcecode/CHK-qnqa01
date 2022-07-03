7b0f880a140caf0bca84309a782e3ccee9011624
sbx_user1051 <sbx_user1051@169.254.105.205>
2022-07-03 14:08:06 +0000

    Sun Jul 03 14:08:06 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    if type(skus) != str:
+        return -1
+
+    available_skus = {
+        
+    }
+
     return -1
+
 
