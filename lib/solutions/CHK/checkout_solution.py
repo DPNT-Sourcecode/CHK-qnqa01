03c2608ccdb5950943b12f646cc52faf91d078d1
sbx_user1051 <sbx_user1051@169.254.248.45>
2022-07-09 16:00:42 +0000

    Sat Jul 09 16:00:42 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -38,16 +38,19 @@ def checkout(skus):
         if key in n_any_of_list_for_x['items']:
             any_of_list.append((key, val))
             total_n_of_any_count += val
-    any_of_list = sorted(any_of_list, key=lambda i: sku_prices[i[0]], reverse=True)
-    any_of_list_str = ''.join([i[0] * i[1] for i in any_of_list])
     
-    offer_applied_times = total_n_of_any_count // n_any_of_list_for_x['num']
 
-    final_sum += offer_applied_times * n_any_of_list_for_x['price']
-    
-    rest_str = any_of_list_str[total_n_of_any_count:]
-    for rest_char in rest_str:
-        final_sum += sku_prices[rest_char]
+    if len(any_of_list):
+        offer_applied_times = total_n_of_any_count // n_any_of_list_for_x['num']
+
+        final_sum += offer_applied_times * n_any_of_list_for_x['price']
+        
+        # the most expensive products should be applied first
+        any_of_list = sorted(any_of_list, key=lambda i: sku_prices[i[0]], reverse=True)
+        any_of_list_str = ''.join([i[0] * i[1] for i in any_of_list])
+        rest_str = any_of_list_str[total_n_of_any_count:]
+        for rest_char in rest_str:
+            final_sum += sku_prices[rest_char]
 
     # apply n_for_x_offers 
     for key, val in sku_groups.items():
@@ -74,6 +77,7 @@ def checkout(skus):
             print(f'Finale sum after {key} key is {final_sum}')
 
     return final_sum
+
 
 
 
