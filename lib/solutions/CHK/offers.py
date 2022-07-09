78d79b27164a7b97de247e195a8c09a938e5b524
sbx_user1051 <sbx_user1051@169.254.248.45>
2022-07-09 15:27:42 +0000

    Sat Jul 09 15:27:42 UTC 2022

--- lib/solutions/CHK/offers.py
+++ lib/solutions/CHK/offers.py
@@ -3,36 +3,36 @@ from enum import Enum
 class Offer(Enum):
     TakeFree = 'take_free',
     NForX = 'n_for_x',
-    AnyOfNForX = 'n_any_of_list_for_x'
+    NAnyOfListForX = 'n_any_of_list_for_x'
 
 sku_prices = {
-        'A': 50,
-        'B': 30,
-        'C': 20,
-        'D': 15,
-        'E': 40,
-        'F': 10,
-        'G': 20,
-        'H': 10,
-        'I': 35,
-        'J': 60,
-        'K': 80,
-        'L': 90,
-        'M': 15,
-        'N': 40,
-        'O': 10,
-        'P': 50,
-        'Q': 30,
-        'R': 50,
-        'S': 30,
-        'T': 20,
-        'U': 40,
-        'V': 50,
-        'W': 20,
-        'X': 90,
-        'Y': 10,
-        'Z': 50
-    }
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+    'E': 40,
+    'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50
+}
 '''
 +------+-------+------------------------+
 | Item | Price | Special offers         |
@@ -154,6 +154,13 @@ take_free_offers = {
         }
     ]
 }
+
+n_any_of_list_for_x = {
+    'items': ['S', 'T', 'X', 'Y', 'Z'],
+    'num': 3,
+    'price': 45
+}
+
 
 
 
